13b80ecb10c96b520bd1ab4fbdddf8adab8c85c9
Alex <alexandremelano@yahoo.com>
2018-07-30 19:44:34 -0400

    another fix attemp

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-  secret:  process.env.SECRET,
+  secret:  'some value',
   resave: true,
   saveUninitialized: false
 }));
